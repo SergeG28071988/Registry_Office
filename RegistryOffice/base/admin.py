091e8801fba7b55e1feb20c48a2e3447e0ac9d22
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-05 11:05:55 +0300

    Создали модели, выполнили миграции, создали пользователя

--- RegistryOffice/base/admin.py
+++ RegistryOffice/base/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Marriage, Divorce
+
+class MarriageAdmin(admin.ModelAdmin):
+    pass
+
+
+class DivorceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Marriage, MarriageAdmin)
+admin.site.register(Divorce, DivorceAdmin)
--- RegistryOffice/base/models.py
+++ RegistryOffice/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Marriage(models.Model):
+    groom = models.CharField(max_length=100)
+    bride = models.CharField(max_length=100)
+    marriage_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.groom} and {self.bride} ({self.marriage_date})'
+
+class Divorce(models.Model):
+    husband = models.CharField(max_length=100)
+    wife = models.CharField(max_length=100)
+    divorce_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.husband} and {self.wife} ({self.divorce_date})'
